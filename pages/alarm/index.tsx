4a08b92cebd77f03f20d8929bf36d4b555f8825c
songyi <jrl103@naver.com>
2023-12-12 22:09:22 +0900

    [Feature] 알림페이지 반응형 적용 #23

--- pages/alarm/index.tsx
+++ pages/alarm/index.tsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 import { rem } from 'polished';
 import { PageMainTitle } from '../../styles/ts/components/titles';
-import AlarmListItem from '../../components/contents/alarm/AlarmListItem';
+import { ImageBox } from '../../styles/ts/components/box';
+import { prefix } from '../../constants/prefix';
+import { BlackColor, FontSizeLg, FontSizeSpSm } from '../../styles/ts/common';
+import { pxToRem } from '../../utils/formatter';
 
 export default function AlarmPage() {
 	// // 서버에서 현재 로그인한 유저의 알림 데이터 가져오기
@@ -15,14 +18,12 @@ export default function AlarmPage() {
 			alarmId: '',
 			alarmTitle: '매칭 취소',
 			alarmContent: '의 매칭이 취소 되었습니다.',
-			pageTitle: '글제목1',
 			pageURL: '',
 		},
 		{
 			alarmId: '',
 			alarmTitle: '경기 디데이!',
 			alarmContent: '의 경기날입니다!',
-			pageTitle: '글제목2',
 			pageURL: '',
 		},
 	];
@@ -33,8 +34,20 @@ export default function AlarmPage() {
 				<PageMainTitle>알림</PageMainTitle>
 			</PageTitleArea>
 			<AlarmList>
-				{gottenAlarms.map((_, i) => {
-					return <AlarmListItem key={gottenAlarms[i].alarmId} alarmTestData={gottenAlarms[i]} />;
+				{gottenAlarms.map((item, index) => {
+					return (
+						<>
+							<AlarmListItem key={'alarm' + index}>
+								<div className='left-box'>
+									<p className='title'>{ item.alarmTitle }</p>
+									<p className='contents'>{ item.alarmContent }</p>
+								</div>
+								<ImageBox width={'40px'} height={'40px'}>
+									<img src={`${prefix}/svg/bell-icon.svg`} alt='알림' />
+								</ImageBox>
+							</AlarmListItem>
+						</>
+					);
 				})}
 			</AlarmList>
 		</>
@@ -42,10 +55,35 @@ export default function AlarmPage() {
 }
 
 const PageTitleArea = styled.div`
-	margin: ${rem('50px')} auto;
+	margin: 50px auto;
 `;
 
 const AlarmList = styled.ul`
 	width: 100%;
-	min-height: rem(calc(100vh - ${rem('80px')}));
 `;
+
+const AlarmListItem = styled.li`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: ${(props) => (props.theme.isResponsive ? `20px ${pxToRem('20px')}` : `20px ${rem('20px')}`)};
+	border-radius: 20px;
+	border: 1px solid #DCDCDC;
+	background: #F9F9F9;
+	margin-bottom: 20px;
+	div.left-box {
+		p {
+			&.title {
+				font-family: Pretendard-Bold;
+				color: ${BlackColor};
+				font-size: ${(props) => (props.theme.isResponsive ? pxToRem(FontSizeLg) : rem(FontSizeLg))};
+				margin-bottom: 5px;
+			}
+			&.contents {
+				font-family: Pretendard-Regular;
+				color: ${BlackColor};
+				font-size: ${(props) => (props.theme.isResponsive ? pxToRem(FontSizeSpSm) : rem(FontSizeSpSm))};
+			}
+		}
+	}
+`
