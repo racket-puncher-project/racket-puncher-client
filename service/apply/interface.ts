2a0c438b91fc443bfecaa51f2295ae88d47cba78
songyi <jrl103@naver.com>
2024-02-11 15:46:28 +0900

    매칭상세 신청수락, 신청취소 api 연동, 드래그앤드롭 조건 분기 #121

--- service/apply/interface.ts
+++ service/apply/interface.ts
@@ -1,6 +1,6 @@
 // 참가 신청 수락(알림)
 
 export interface reqApplyMatchesApiData {
-	pendingApplies: []; // 신청 인원 리스트
-	acceptedApplies: []; // 참여 인원 리스트
+	pendingApplies: any[]; // 신청 인원 리스트
+	acceptedApplies: any[]; // 참여 인원 리스트
 }
