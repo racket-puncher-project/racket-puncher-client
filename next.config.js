64067e60ea2c4ed0dfb73bf80cfec091ae023b51
syji <jrl10346@gmail.com>
2024-03-26 14:13:31 +0900

    [Test] 환경에 따른 이미지 경로 테스트 #172

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-	basePath: '/racket-puncher-client',
-	assetPrefix: 'https://racket-puncher-project.github.io/racket-puncher-client/',
+	// basePath: '/racket-puncher-client',
+	// assetPrefix: 'https://racket-puncher-project.github.io/racket-puncher-client/',
+	basePath: process.env.NODE_ENV === 'production' ? '/racket-puncher-client/' : '',
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/racket-puncher-client/' : '',
 	env: {
 		KAKAO_MAPS_APP_KEY: '20bd45f114aff9ec8fee730fe2453f5c',
+		ASSET_PREFIX: 'https://racket-puncher-project.github.io/racket-puncher-client',
 	},
 	webpack: (config) => {
 		// `net` 모듈이 요구되는 경우 빈 객체로 대체
