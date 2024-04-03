6fa11e253b4ef1029adf5e8c41eaa4a860490f27
songyi <jrl103@naver.com>
2024-04-03 17:48:53 +0900

    [Feature] 참여 수락 로직 변경 #195

--- components/contents/chat/ChatRoom.tsx
+++ components/contents/chat/ChatRoom.tsx
@@ -5,27 +5,16 @@ import { prefix } from '../../../constants/prefix';
 import { pxToRem } from '../../../utils/formatter';
 import { rem } from 'polished';
 import ChatListModal from './ChatListModal';
-import ChatService from '../../../service/chat/service';
 
 export default function ChatRoom() {
 	const [isChatDrawerOpen, setIsChatDrawerOpen] = useState(false);
 	const showChatDrawer = () => setIsChatDrawerOpen(true);
 	const toggleChatListDrawer = (prev: boolean) => setIsChatDrawerOpen((prev) => !prev);
 
-	// 채팅방 목록 불러오기
-	const getChatListData = async () => {
-		try {
-			const res = await ChatService.getChatList();
-			console.log('res', res.data);
-		} catch (e) {
-			console.log(e);
-		}
-	};
-
 	const clickChatRoomBtn = () => {
-		getChatListData();
 		showChatDrawer();
 	};
+
 	return (
 		<>
 			<ChatListModal isOpen={isChatDrawerOpen} toggleDrawer={toggleChatListDrawer} />
